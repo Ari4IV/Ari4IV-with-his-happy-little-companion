3dae6352d02be61df0334ad6308843fabde32d67
Ari4IV <32978451+Ari4IV@users.noreply.github.com>
2023-12-09 17:31:28 +0800

    Arduino To Mqtt

--- Arduino/mqttController/bridge-server.js
+++ Arduino/mqttController/bridge-server.js
@@ -10,8 +10,10 @@ const port = '/dev/tty.usbmodem1301';
 const serial = new SerialPort({ path: port, baudRate: 9600 });
 const mqttClient = mqtt.connect(mqttServer);
 
+const ARDUINO_PREFIX = "FromArduino:";  // 來自 Arduino 的訊息前綴
+
 const receiveParser = (message) => {
-    // arduinoToMqtt(message);
+    arduinoToMqtt(message);
 }
 
 const sentToArduino = (message) => {
@@ -19,13 +21,26 @@ const sentToArduino = (message) => {
 }
 
 const mqttToArduino = (message) => {
-    console.log({mqttToArduino: message});
-    sentToArduino(message);
+    // 只有當訊息不是來自 Arduino 時才發送
+    if (!message.startsWith(ARDUINO_PREFIX)) {
+        console.log({mqttToArduino: message});
+        sentToArduino(message);
+    }
 }
 
 const arduinoToMqtt = (message) => {
-    console.log({arduinoToMqtt: message});
-    mqttClient.publish(subscribeTopic, message);
+    // 只處理有效的訊息
+    if (messageIsValid(message)) {
+        console.log({arduinoToMqtt: message});
+        // 添加前綴到訊息
+        const messageWithPrefix = ARDUINO_PREFIX + message;
+        mqttClient.publish(subscribeTopic, messageWithPrefix);
+    }
+}
+
+function messageIsValid(message) {
+    // 實現一個檢查訊息是否有效的邏輯
+    return message && !message.startsWith('\r') && !message.startsWith('Echo:');
 }
 
 mqttClient.on('connect', () => {
